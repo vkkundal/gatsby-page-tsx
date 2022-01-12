c9480ae936db6f7cbad54194cc97efdd93e34f21
vinod <vinodkumar1805@gmail.com>
2022-01-12 17:57:07 +0530

    first commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,43 @@
 import * as React from "react"
 import PropTypes from "prop-types"
+import { StaticImage } from "gatsby-plugin-image"
+import "bootstrap/dist/css/bootstrap.min.css"
 import { Link } from "gatsby"
+import styles from "../styles/header.module.css"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+
+const Header = ({ siteTitle, children }) => (
+  <div class="container-fluid">
+  <div class="row">
+    <div class="col">
+    <StaticImage
+      src="../images/logo-light-img.png"     
+      alt="A Gatsby astronaut"      
+    />
     </div>
-  </header>
+    <div class="col">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>        
+      </ul>
+    </div>
+    <div class="col">
+      3 of 3
+    </div>
+  </div>
+</div>
 )
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+// Header.propTypes = {
+//   siteTitle: PropTypes.string,
+// }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+// Header.defaultProps = {
+//   siteTitle: ``,
+// }
 
 export default Header
